f43cf913826586ace3d39be271be7c5e08644f83
Vaishnavirajnarayanan <vaishnavirajnarayanan@gmail.com>
2026-06-22 21:06:30 +0530

    Fix CORS configuration

--- src/main/java/com/example/hellobackend/HelloController.java
+++ src/main/java/com/example/hellobackend/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="http://localhost:3002")
+@CrossOrigin(origins = "*")
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
